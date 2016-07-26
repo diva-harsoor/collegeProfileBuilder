4188e5e6fb8b4e47f1c5ce9d22a25508578ac18e
Diva Harsoor <divaharsoor@divas-macbook-air.local>
2016-07-25 22:14:21 -0500

    Allow entries to be added

--- CollegeProfileBuilder/ViewController.swift
+++ CollegeProfileBuilder/ViewController.swift
@@ -26,5 +26,27 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.textLabel?.text = colleges[indexPath.row].name
         return cell
     }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            colleges.removeAtIndex(indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    @IBAction func onTappedPlusButton(sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add City", message: nil, preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Add City Here"
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alert.addAction(cancelAction)
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (action) in
+            let collegeTextField = alert.textFields![0] as UITextField
+            self.colleges.append(College(name: collegeTextField.text!))
+            self.tableView.reloadData()
+        }
+        alert.addAction(addAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 }
 
